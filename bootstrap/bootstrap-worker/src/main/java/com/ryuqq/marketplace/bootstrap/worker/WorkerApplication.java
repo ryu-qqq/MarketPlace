8329864ed69348e52a71bddc20fdba7a40bb3742
sangwon-ryu <fbtkdals2@naver.com>
2026-02-26 18:31:45 +0900

    fix: 메인 DataSource/EMF/TM을 @Primary로 명시 정의하여 Legacy 모듈 공존 문제 해결
    LegacyJpaConfig가 DataSource와 LocalContainerEntityManagerFactoryBean을 생성하면
    Spring Boot auto-config의 @ConditionalOnMissingBean 조건으로 메인 빈이 생성되지 않는 문제.
    MainJpaConfig에서 @Primary로 메인 인프라 빈을 명시 정의하고,
    부트스트랩의 entityManagerFactoryRef 지정을 제거 (Primary로 자동 해상).

--- bootstrap/bootstrap-worker/src/main/java/com/ryuqq/marketplace/bootstrap/worker/WorkerApplication.java
+++ bootstrap/bootstrap-worker/src/main/java/com/ryuqq/marketplace/bootstrap/worker/WorkerApplication.java
@@ -27,8 +27,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         })
 @EnableJpaRepositories(
         basePackages = "com.ryuqq.marketplace.adapter.out.persistence",
-        entityManagerFactoryRef = "entityManagerFactory",
-        transactionManagerRef = "transactionManager",
         excludeFilters =
                 @ComponentScan.Filter(
                         type = FilterType.REGEX,
