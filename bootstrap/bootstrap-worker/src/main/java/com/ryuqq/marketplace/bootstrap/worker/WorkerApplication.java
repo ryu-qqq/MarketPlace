b5fe5d6084268da465929c98b5e942e642e628ca
sangwon-ryu <fbtkdals2@naver.com>
2026-02-26 17:50:39 +0900

    fix: @EnableJpaRepositories에 entityManagerFactoryRef 명시하여 Legacy EMF 충돌 해결
    Legacy 모듈 로딩 시 EntityManagerFactory가 2개(main + legacy) 존재하게 되어
    메인 리포지토리가 어떤 EMF를 사용할지 결정 못하는 문제 수정.
    3개 부트스트랩 모두 entityManagerFactoryRef + transactionManagerRef 명시.

--- bootstrap/bootstrap-worker/src/main/java/com/ryuqq/marketplace/bootstrap/worker/WorkerApplication.java
+++ bootstrap/bootstrap-worker/src/main/java/com/ryuqq/marketplace/bootstrap/worker/WorkerApplication.java
@@ -27,6 +27,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         })
 @EnableJpaRepositories(
         basePackages = "com.ryuqq.marketplace.adapter.out.persistence",
+        entityManagerFactoryRef = "entityManagerFactory",
+        transactionManagerRef = "transactionManager",
         excludeFilters =
                 @ComponentScan.Filter(
                         type = FilterType.REGEX,
