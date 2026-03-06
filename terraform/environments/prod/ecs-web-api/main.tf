34b89909a4d6c34dfe6df87d9b9bf4a647688bef
sangwon-ryu <fbtkdals2@naver.com>
2026-03-06 17:02:38 +0900

    feat: prod ECS 오토스케일링 설정 (api max 2→3, worker 신규 min1/max3)

--- terraform/environments/prod/ecs-web-api/main.tf
+++ terraform/environments/prod/ecs-web-api/main.tf
@@ -468,7 +468,7 @@ module "ecs_service" {
   # Auto Scaling
   enable_autoscaling        = true
   autoscaling_min_capacity  = 1
-  autoscaling_max_capacity  = 2
+  autoscaling_max_capacity  = 3
   autoscaling_target_cpu    = 70
   autoscaling_target_memory = 80
 
